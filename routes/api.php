12725e35d4fd4bc0772cb61e3e7d83b744399a11
Angga Pratama <angga.pratama1923@gmail.com>
2023-04-12 01:18:46 +0700

    sanctum authorization

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [PostsController::class, 'index']);
-Route::get('/posts/{id}', [PostsController::class, 'show']);
+Route::get('/posts', [PostsController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [PostsController::class, 'show'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware(['auth:sanctum']);
+Route::get('/user_login', [AuthenticationController::class, 'user_login'])->middleware(['auth:sanctum']);
